094e684eb0ccafbd4591607e1478c7697cb40644
apurbo <absarker238@gmail.com>
2025-10-22 13:02:15 -0700

    added my crazy loagin

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -1,13 +1,13 @@
 import Lottie from 'lottie-react'
 import React from 'react'
 import loading from '../../src/assets/Lottie/crazy_loading.json'
-export default function Loading() {
-    return (
-        <div className='h-screen flex flex-col justify-center items-center'>
-            <div className='h-20 w-20'>
-                <Lottie animationData={loading}></Lottie>
-            </div>
 
-        </div>
-    )
+export default function Loading() {
+  return (
+    <div className='h-[calc(100vh-64px)] flex flex-col justify-center items-center'>
+      <div className='h-26 w-28'>
+        <Lottie animationData={loading} loop={true} />
+      </div>
+    </div>
+  )
 }
--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -11,153 +11,157 @@ const Navbar = () => {
   return (
     <div className="bg-linear-to-br fixed z-50 top-0 left-0 w-full bg-white/75 backdrop-blur-lg">
       <div
-      className="navbar  z-50 md:px-6 px-3 py-3 
+        className="navbar  z-50 md:px-6 px-3 py-3 
         container mx-auto
       border-b border-gray-300/60 text-gray-800"
-    >
-      {/* Logo Section */}
-      <div className="flex-1 flex flex-row items-center  space-x-2 relative">
+      >
+        {/* Logo Section */}
+        <div className="flex-1 flex flex-row items-center  space-x-2 relative">
 
-        <div
-          onClick={() => {
-            setIcon(!icon);
-            setOpen(!open);
-          }}
-          className="cursor-pointer md:hidden rounded-md hover:bg-gray-200 transition"
-        >
-          {icon ? (
-            <IoMdMenu className="text-2xl text-gray-700" />
-          ) : (
-            <IoMdClose className="text-2xl text-gray-700" />
+          <div
+            onClick={() => {
+              setIcon(!icon);
+              setOpen(!open);
+            }}
+            className="cursor-pointer md:hidden rounded-md hover:bg-gray-200 transition"
+          >
+            {icon ? (
+              <IoMdMenu className="text-2xl text-gray-700" />
+            ) : (
+              <IoMdClose className="text-2xl text-gray-700" />
+            )}
+          </div>
+
+          <Link to='/' className="text-2xl flex items-center "><img className="w-14" src="https://i.ibb.co.com/VcWCWLf5/logo.png" alt="logo" />
+            <h1 className="text-2xl font-extrabold text-indigo-700 tracking-wide">
+              SkillSwap
+            </h1>
+          </Link>
+          {/* Dropdown Items */}
+          {open && (
+            <ul className="absolute left-0 top-9 bg-base-100 rounded-box z-50 w-40 p-2 shadow-lg border border-gray-200">
+              <li>
+                <a className="block px-3 py-2 rounded-md hover:bg-gray-100 cursor-pointer">
+                  Item 1
+                </a>
+              </li>
+              <li>
+                <a className="block px-3 py-2 rounded-md hover:bg-gray-100 cursor-pointer">
+                  Item 2
+                </a>
+              </li>
+            </ul>
           )}
         </div>
 
-        <Link to='/' className="text-2xl"><img className="h-14 w-14" src="https://i.ibb.co.com/ccrvFMNB/Dynamic-Skill-Swap-Logo-with-Gears-removebg-preview.png" alt="logo" /></Link>
-        {/* Dropdown Items */}
-        {open && (
-          <ul className="absolute left-0 top-9 bg-base-100 rounded-box z-50 w-40 p-2 shadow-lg border border-gray-200">
-            <li>
-              <a className="block px-3 py-2 rounded-md hover:bg-gray-100 cursor-pointer">
-                Item 1
-              </a>
-            </li>
-            <li>
-              <a className="block px-3 py-2 rounded-md hover:bg-gray-100 cursor-pointer">
-                Item 2
-              </a>
-            </li>
-          </ul>
-        )}
-      </div>
+        {/* Nav Links */}
+        <div className="hidden md:flex gap-4 mr-6">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              `px-3 py-2 rounded-md font-medium transition ${isActive
+                ? "underline underline-offset-4 font-semibold text-blue-600"
+                : " hover:text-blue-600"
+              }`
+            }
+          >
+            Home
+          </NavLink>
 
-      {/* Nav Links */}
-      <div className="hidden md:flex gap-4 mr-6">
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            `px-3 py-2 rounded-md font-medium transition ${isActive
-              ? "underline underline-offset-4 font-semibold text-blue-600"
-              : " hover:text-blue-600"
-            }`
-          }
-        >
-          Home
-        </NavLink>
+          <NavLink
+            to="/profile"
+            className={({ isActive }) =>
+              `px-3 py-2 rounded-md font-medium transition ${isActive
+                ? "underline underline-offset-4 font-semibold text-blue-600"
+                : "hover:text-blue-600"
+              }`
+            }
+          >
+            My Profile
+          </NavLink>
 
-        <NavLink
-          to="/profile"
-          className={({ isActive }) =>
-            `px-3 py-2 rounded-md font-medium transition ${isActive
-              ? "underline underline-offset-4 font-semibold text-blue-600"
-              : "hover:text-blue-600"
-            }`
-          }
-        >
-          My Profile
-        </NavLink>
+        </div>
 
-      </div>
-
-      {/* User Section */}
-      <div className="flex items-center gap-3">
-        {user ? (
-          <>
-            <div className="dropdown dropdown-end">
-              <div
-                tabIndex={0}
-                role="button"
-                className="btn btn-ghost btn-circle tooltip tooltip-bottom avatar border border-gray-300 hover:scale-105 transition"
-                data-tip={user.displayName}
-              >
-                <div className="w-10 rounded-full">
-                  <img
-                    src={
-                      user.photoURL ||
-                      "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                    }
-                    alt="User Avatar"
-                  />
-                </div>
-              </div>
-              <ul tabIndex="-1" className="dropdown-content md:hidden menu bg-base-100 rounded-box z-1 w-30 p-2 shadow-sm">
-                <li> <NavLink
-                  to="/profile"
-                  className={({ isActive }) =>
-                    `px-3 py-2 rounded-md font-medium transition ${isActive
-                      ? "underline underline-offset-4 text-blue-600"
-                      : "hover:bg-gray-200/70 hover:text-blue-600"
-                    }`
-                  }
+        {/* User Section */}
+        <div className="flex items-center gap-3">
+          {user ? (
+            <>
+              <div className="dropdown dropdown-end">
+                <div
+                  tabIndex={0}
+                  role="button"
+                  className="btn btn-ghost btn-circle tooltip tooltip-bottom avatar border border-gray-300 hover:scale-105 transition"
+                  data-tip={user.displayName}
                 >
-                  My Profile
-                </NavLink></li>
-                <li>
-                  <button
-                    onClick={()=>logOut()}
-                    className="btn   border-blue-400 text-blue-600 hover:bg-blue-500 hover:text-white transition"
+                  <div className="w-10 rounded-full">
+                    <img
+                      src={
+                        user.photoURL ||
+                        "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                      }
+                      alt="User Avatar"
+                    />
+                  </div>
+                </div>
+                <ul tabIndex="-1" className="dropdown-content md:hidden menu bg-base-100 rounded-box z-1 w-30 p-2 shadow-sm">
+                  <li> <NavLink
+                    to="/profile"
+                    className={({ isActive }) =>
+                      `px-3 py-2 rounded-md font-medium transition ${isActive
+                        ? "underline underline-offset-4 text-blue-600"
+                        : "hover:bg-gray-200/70 hover:text-blue-600"
+                      }`
+                    }
                   >
-                    Logout
-                  </button>
-                </li>
-              </ul>
+                    My Profile
+                  </NavLink></li>
+                  <li>
+                    <button
+                      onClick={() => logOut()}
+                      className="btn   border-blue-400 text-blue-600 hover:bg-blue-500 hover:text-white transition"
+                    >
+                      Logout
+                    </button>
+                  </li>
+                </ul>
 
-            </div>
+              </div>
 
-            <button
-              onClick={()=>logOut()}
-              className="btn btn-primary border-blue-400 text-white hover:bg-blue-500 hover:text-white transition"
-            >
-              Logout
-            </button>
-          </>
-        ) : (
-          <>
-            <NavLink
-              to="/login"
-              className={({ isActive }) =>
-                `px-3 py-2 rounded-md font-medium transition ${isActive
-                  ? "underline font-semibold text-blue-600"
-                  : "hover:text-blue-600"
-                }`
-              }
-            >
-              Login
-            </NavLink>
-            <NavLink
-              to="/signUp"
-              className={({ isActive }) =>
-                `px-3 py-2 rounded-md font-medium transition ${isActive
-                  ? "underline font-semibold text-blue-600"
-                  : "hover:text-blue-600"
-                }`
-              }
-            >
-              Sign Up
-            </NavLink>
-          </>
-        )}
+              <button
+                onClick={() => logOut()}
+                className="btn btn-primary border-blue-400 text-white hover:bg-blue-500 hover:text-white transition"
+              >
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <NavLink
+                to="/login"
+                className={({ isActive }) =>
+                  `px-3 py-2 rounded-md font-medium transition ${isActive
+                    ? "underline font-semibold text-blue-600"
+                    : "hover:text-blue-600"
+                  }`
+                }
+              >
+                Login
+              </NavLink>
+              <NavLink
+                to="/signUp"
+                className={({ isActive }) =>
+                  `px-3 py-2 rounded-md font-medium transition ${isActive
+                    ? "underline font-semibold text-blue-600"
+                    : "hover:text-blue-600"
+                  }`
+                }
+              >
+                Sign Up
+              </NavLink>
+            </>
+          )}
+        </div>
       </div>
-    </div>
     </div>
   );
 };
