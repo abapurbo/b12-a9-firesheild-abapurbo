71fcc729ddf51e3a3405a23ce6347c0f21786c24
apurbo <absarker238@gmail.com>
2025-10-22 23:23:10 -0700

    added footer section

--- src/Config/firebase.config.js
+++ src/Config/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDKmkpcKHt9ZIlgteXWbKU_zO9A6gcvnr8",
-  authDomain: "b12-a9-firesheild-abapurbo.firebaseapp.com",
-  projectId: "b12-a9-firesheild-abapurbo",
-  storageBucket: "b12-a9-firesheild-abapurbo.firebasestorage.app",
-  messagingSenderId: "11139341639",
-  appId: "1:11139341639:web:734b0b970021eb7bd40062"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
